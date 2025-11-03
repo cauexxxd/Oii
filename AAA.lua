d2ac98c34498160f576544aea2d33896bb13a75c
Cauê.zx <caue61075@gmail.com>
2025-11-03 00:44:55 -0300

    Update AAA.lua

--- AAA.lua
+++ AAA.lua
@@ -154,3 +154,5 @@
 	Misc = {["Hide Chat"] = false,
 		["Hide Leaderboard"] = false,
 		["Highlight Mode"] = false
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Footagesus/WindUI/refs/heads/main/main_example.lua'))()
